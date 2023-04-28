e6a2f2c0d2e0f0b4f58feb4a863b81f6eb970d74
GeekyNerd <85250765+BloodShoT14@users.noreply.github.com>
2023-04-28 19:26:30 +0530

    Create largest_element_in_array.py

---
+++ largest_element_in_array.py
@@ -0,0 +1,14 @@
+if __name__ == '__main__':
+    arr = map(int,input().split(','))
+    ########## METHOD 1 ##########
+    # max_elem = -999
+    # for i in arr:
+    #     if i > max_elem:
+    #         max_elem = i
+    # print(max_elem)
+
+    ########## METHOD 1 ##########
+    # print(max(arr))
+
+    ########## METHOD 1 ##########
+    # print(sorted(arr)[-1])
