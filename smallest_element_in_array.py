db3dc83f7b0e53e9fbd93ddd4f8fe71149ec9971
GeekyNerd <85250765+BloodShoT14@users.noreply.github.com>
2023-04-28 19:33:28 +0530

    Create smallest_element_in_array.py

---
+++ smallest_element_in_array.py
@@ -0,0 +1,14 @@
+if __name__ == '__main__':
+    arr = map(int,input().split(','))
+    ########## METHOD 1 ##########
+    # min_elem = 99999
+    # for i in arr:
+    #     if i < min_elem:
+    #         min_elem = i
+    # print(min_elem)
+
+    ########## METHOD 1 ##########
+    # print(min(arr))
+
+    ########## METHOD 1 ##########
+    # print(sorted(arr)[0])
