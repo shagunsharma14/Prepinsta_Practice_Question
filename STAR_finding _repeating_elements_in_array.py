c37bb8645af17fcf6226658a24c6777234ecb9a9
Shagun Sharma <shaguns880@gmail.com>
2023-05-05 20:53:44 +0530

    finding repeating elm in array

---
+++ STAR_finding _repeating_elements_in_array.py
@@ -0,0 +1,14 @@
+if __name__ == '__main__':
+    arr = list(map(int,input().split(',')))
+    n = len(arr)
+    visited = [False for _ in range(len(arr))]
+    for i in range(len(arr)):
+        if(visited[i] == True):
+            continue
+        count = 1
+        for j in range(i+1,n):
+                if(arr[i]==arr[j]):
+                    visited[j] = True
+                    count += 1
+        if count != 1:
+            print(arr[i])
