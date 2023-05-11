82a3572ceacf963c6ae7042965f3147ac25d3f85
Shagun Sharma <shaguns880@gmail.com>
2023-05-11 16:30:12 +0530

    program added

---
+++ Finding Minimum sum of absolute difference of given array.py
@@ -0,0 +1,17 @@
+import sys
+
+def sumOfMinAbsDifferences(arr,n):
+    result = sys.maxsize
+    
+    for i in range(0,n):
+        sum = 0
+        for j in range(0, n):
+            sum += abs(arr[i]-arr[j])
+        result = min(result, sum)
+    return result
+         
+ 
+# Driver code
+arr = [2, 5, 4, 3]
+n = len(arr)
+print( "Required Sum = ", sumOfMinAbsDifferences(arr, n))
