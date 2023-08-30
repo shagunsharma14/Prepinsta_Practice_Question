7bc828ea2ebb72e5a099f1a094e10f0f552e2615
Shagun Sharma <85250765+shagunsharma14@users.noreply.github.com>
2023-08-31 02:14:32 +0530

    Update STAR_prime_number_in_range.py

--- STAR_prime_number_in_range.py
+++ STAR_prime_number_in_range.py
@@ -1,19 +1,12 @@
-if __name__ == '__main__':
-    low, high = input().split()
-    prime_nums = []
-    if int(low) == 2:
-        prime_nums.append(int(low))
-    for num in range(int(low), int(high)+1):
-        flag = 0
-        if num<2:
-            flag = 1
-        if num%2==0:
-            continue
-        else:
-            for i in range(2,int(num/2)+1):
-                if num%i==0:
-                    flag=1
-                    break
-            if flag==0:
-                prime_nums.append(num)
-    print(prime_nums)
+def is_prime(num):
+    if num <= 1:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+low, high = map(int, input("Enter the range (low and high): ").split())
+prime_numbers = [num for num in range(low, high + 1) if is_prime(num)]
+
+print("Prime numbers in the given range:", prime_numbers)
