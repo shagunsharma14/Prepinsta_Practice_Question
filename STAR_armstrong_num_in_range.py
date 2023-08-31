415e011cc575cd1523fe7857b75092e5e36c30c7
Shagun Sharma <85250765+shagunsharma14@users.noreply.github.com>
2023-08-31 22:28:20 +0530

    Update STAR_armstrong_num_in_range.py

--- STAR_armstrong_num_in_range.py
+++ STAR_armstrong_num_in_range.py
@@ -1,13 +1,13 @@
-if __name__ == '__main__':
-    low, high = input().split()
-    for num in range(int(low), int(high)+1):
-        sum_is = 0
-        num_len = len(str(num))
-        temp = num
-        while temp>0:
-            digit  = temp%10
-            sum_is += pow(digit, num_len)
-            temp = temp//10
-
-            if num == sum_is:
-                print(num, end=', ')
+def armstrong(num):
+#     temp = num
+#     res = 0
+#     while temp!=0:
+#         rem = int(temp%10)
+#         res = res+pow(rem,len(str(num)))
+#         temp//=10
+#     if num==res:
+#         print(f'{num} is Armstrong')
+    
+# low, high = map(int, input().split())
+# for i in range(low, high+1):
+#     armstrong(i)
