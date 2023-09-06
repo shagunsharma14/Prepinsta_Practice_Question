e90d0b35d3dd22b97e9551644b4b3a89e03ac063
Shagun Sharma <85250765+shagunsharma14@users.noreply.github.com>
2023-09-06 20:11:53 +0530

    Update STAR_sorting_elements_of_array_by_frequency .py

--- STAR_sorting_elements_of_array_by_frequency .py
+++ STAR_sorting_elements_of_array_by_frequency .py
@@ -1,22 +1,15 @@
-# arr = [1, 2, 3, 2, 3, 3, 4, 4, 4, 4]
-# arr = [10, 20, 10, 10, 20, 30, 30, 30, 30, 0]
-
-freq = {}
-for i in arr:
-    if i in freq:
-        freq[i] += 1
-    else:
-        freq[i] = 1
-
-sorted_arr = []
-while freq:
+def sort_by_frequency(arr):
+    dic = {}
+    for i in arr:
+        if i not in dic:
+            dic[i] = 1
+        else:
+            dic[i]+=1
     
-    max_key = None
-    for key in freq:
-        if max_key is None or freq[key] > freq[max_key]:
-            max_key = key
-    for i in range(freq[max_key]):
-        sorted_arr.append(max_key)
-    del freq[max_key]
-
-print(sorted_arr)
+    ls = []
+    for el,feq in dic.items():
+        ls.append((feq,el))
+    ls.sort(reverse=True)
+    return [el for feq,el in ls]
+a = [4, 2, 2, 8, 3, 3, 1, 1, 1, 8]
+print(sort_by_frequency(a))
